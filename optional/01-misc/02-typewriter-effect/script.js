1024cf06cb320f3dbf8c941daad9bad7a8d997ff
nehahonrao <nehalonkar27@gmail.com>
2021-10-06 15:24:50 +0200

    add it

---
+++ optional/01-misc/02-typewriter-effect/script.js
@@ -0,0 +1,54 @@
+/* becode/javascript
+ *
+ * /07-misc/02-typewriter-effect/script.js - 7.2: effet machine à écrire
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+
+})();
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Hello Its Neha and I am pursuing Web Developer Course and I will be Future Best Web Developer"
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("target");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
