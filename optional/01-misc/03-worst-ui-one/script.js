07789dc139ced981750e210f1d2c4cefa5a173a3
nehahonrao <nehalonkar27@gmail.com>
2021-10-07 16:49:40 +0200

    add

---
+++ optional/01-misc/03-worst-ui-one/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /07-misc/03-worst-ui-one/script.js - 7.3: la pire interface (1) : phone slider
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+var target=document.getElementById("target");
+var slider=document.getElementById("slider");
+
+slider.addEventListener("input",()=>{
+target.innerHTML= "0"+(slider.value); 
+
+});
+
+})();
